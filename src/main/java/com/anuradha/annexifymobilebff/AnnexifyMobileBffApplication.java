892880e10c86fbb4e34fb639c948bb3b77556000
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-10-17 10:40:00 +0530

    Initial setup

---
+++ src/main/java/com/anuradha/annexifymobilebff/AnnexifyMobileBffApplication.java
@@ -0,0 +1,13 @@
+package com.anuradha.annexifymobilebff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnexifyMobileBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnexifyMobileBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuradha/annexifymobilebff/AnnexifyMobileBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuradha.annexifymobilebff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnexifyMobileBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
